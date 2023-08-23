3fbb1d0b29c0ce26ea46b1ae4e82a2b063aee793
Roshan Nyaupane <106418513+RNyaupane@users.noreply.github.com>
2023-08-23 21:55:52 +0545

    Like and disliked function is corrected

--- controller/blogCtrl.js
+++ controller/blogCtrl.js
@@ -34,7 +34,9 @@ const getBlog = asyncHandler(async (req, res) => {
     const { id } = req.params;
     validateMongodbId(id);
     try {
-        const getBlog = await Blog.findById(id);
+        const getBlog = await Blog.findById(id)
+        .populate('likes')
+        .populate('dislikes');
         await Blog.findByIdAndUpdate(id, {
             $inc: { numViews: 1 }
         }, { new: true }
