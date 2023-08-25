21c512f3bd6201402f01f1a0a4288ab1fa754c82
Roshan Nyaupane <106418513+RNyaupane@users.noreply.github.com>
2023-08-25 06:34:50 +0545

    Blog category added

--- models/brandModel.js
+++ models/brandModel.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose'); // Erase if already required
 
 // Declare the Schema of the Mongo model
-var blogCategorySchema = new mongoose.Schema({
+var userSchema = new mongoose.Schema({
     name:{
         type:String,
         required:true,
@@ -25,4 +25,4 @@ var blogCategorySchema = new mongoose.Schema({
 });
 
 //Export the model
-module.exports = mongoose.model('Blog Categories', blogCategorySchema);
+module.exports = mongoose.model('User', userSchema);
--- routes/blogCategoriyRoute.js
+++ routes/blogCategoriyRoute.js
@@ -1,1 +1,14 @@
-const express = require('express')
+const express = require('express');
+const { createCategory, updateCategory, deleteCategory, getaCategory, getAllCategory, } = require('../controller/blogCategoryCtrl');
+const { authMiddleware, isAdmin } = require('../middlewares/authMiddleware');
+const router=express.Router();
+
+
+router.post('/',authMiddleware,isAdmin,createCategory)
+router.put('/:id',authMiddleware,isAdmin, updateCategory)
+router.delete('/:id',authMiddleware,isAdmin, deleteCategory)
+router.get('/:id', getaCategory)
+router.get('/', getAllCategory)
+
+
+module.exports=router;
