c0cc3925b9a40fdc9319829db86d38bbc4529067
Roshan Nyaupane <106418513+RNyaupane@users.noreply.github.com>
2023-08-27 05:40:08 +0545

    Cloudnairy configuration and added image upload function

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -1,6 +1,24 @@
 const cloudnairy = require('cloudinary');
+
+
 cloudnairy.config({
     cloud_name: process.env.CLOUD_NAME, 
     api_key: process.env.API_KEY, 
     api_secret: process.env.API_SECRET 
-})
+});
+
+
+
+const cloudnairyUploadImg = async(fileToUploads) => {
+    return new Promise ((resolve) =>{
+        cloudnairy.uploader.upload(fileToUploads, (result)=>{
+            resolve({
+                url:result.secure_url,
+            },{
+                resource_type:"auto",
+            })
+        })
+    })
+}
+
+module.exports = cloudnairyUploadImg;
