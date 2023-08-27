102c7c13d17348c895ecd0ada8251f579f5e0c8a
Roshan Nyaupane <106418513+RNyaupane@users.noreply.github.com>
2023-08-27 06:30:17 +0545

    Image Resize functionality added for product and blog images

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -2,20 +2,20 @@ const cloudnairy = require('cloudinary');
 
 
 cloudnairy.config({
-    cloud_name: process.env.CLOUD_NAME, 
-    api_key: process.env.API_KEY, 
-    api_secret: process.env.API_SECRET 
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET
 });
 
 
 
-const cloudnairyUploadImg = async(fileToUploads) => {
-    return new Promise ((resolve) =>{
-        cloudnairy.uploader.upload(fileToUploads, (result)=>{
+const cloudnairyUploadImg = async (fileToUploads) => {
+    return new Promise((resolve) => {
+        cloudnairy.uploader.upload(fileToUploads, (result) => {
             resolve({
-                url:result.secure_url,
-            },{
-                resource_type:"auto",
+                url: result.secure_url,
+            }, {
+                resource_type: "auto",
             })
         })
     })
